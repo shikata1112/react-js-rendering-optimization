5baf69e089838defa597155b59a69c339d30f79f
Shu Shikata <shu.42111227@gmail.com>
2022-02-05 15:31:26 +0900

    memo, useCallbackを使用してレンダリングを最適化した

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,12 +1,14 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
-  const { open } = props;
-  console.log('ChildAreaがレンダリングされた！')
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンダリングされた！");
 
   const data = [...Array(2000).keys()];
   data.forEach(() => {
@@ -17,8 +19,9 @@ export const ChildArea = (props) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
